27e95d72245c7e4fe3a74645a94a44fb87adae74
Peter Bryant <bryantpw@bethanylb.edu>
2021-12-17 09:33:19 -0600

    new dataframe including only the data to save

--- get-hist-libcomcat.py
+++ get-hist-libcomcat.py
@@ -15,7 +15,20 @@ summary_events = search(starttime=datetime(1994, 1, 17, 12, 30), endtime=datetim
 sum_df = get_summary_data_frame(summary_events)
 
 # rename magnitude
-sum_df.rename(columns={'magnitude': 'mag'}, inplace=True)
+#sum_df.rename(columns={'magnitude': 'mag'}, inplace=True)
+
+# Create new dataframe with only the data to save
+#   convert to central time and make time the index
+new_time_index = pd.DatetimeIndex(pd.to_datetime(sum_df['time'] )).tz_localize('UTC').tz_convert('US/Central')
+
+longitude = sum_df['longitude'].to_numpy()
+latitude = sum_df['latitude'].to_numpy()
+magnitude = sum_df['magnitude'].to_numpy()
+location_st = sum_df['location'].to_numpy()
+
+data_to_save = {'latitude':latitude, 'longitude':longitude, 'magnitude':magnitude, 'location string':location_st}
+eq_data = pd.DataFrame(data=data_to_save,
+                       index=new_time_index)
 
 # pickle it
-sum_df.to_pickle('./historyDF.pkl')
+eq_data.to_pickle('./historyDF.pkl')
