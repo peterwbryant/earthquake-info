5fca50de84c46017adf237955cf805a0c5b90f35
Peter Bryant <bryantpw@bethanylb.edu>
2022-01-10 10:03:30 -0600

    added info database

--- get-hist-libcomcat.py
+++ get-hist-libcomcat.py
@@ -18,14 +18,23 @@ sum_df = get_summary_data_frame(summary_events)
 #   convert to central time and make time the index
 new_time_index = pd.DatetimeIndex(pd.to_datetime(sum_df['time'] )).tz_localize('UTC').tz_convert('US/Central')
 
+# data table
 longitude = sum_df['longitude'].to_numpy()
 latitude = sum_df['latitude'].to_numpy()
 magnitude = sum_df['magnitude'].to_numpy()
-#location_st = sum_df['location'].to_numpy()
+
+# info table
+location_st = sum_df['location'].to_numpy()
+url_st = sum_df['url'].to_numpy()
 
 data_to_save = {'latitude':latitude, 'longitude':longitude, 'magnitude':magnitude}
 eq_data = pd.DataFrame(data=data_to_save,
                        index=new_time_index)
 
-# pickle it
+info_to_save = {'magnitude':magnitude, 'location':location_st, 'USGS event page':url_st}
+eq_info = pd.DataFrame(data=info_to_save,
+                       index=new_time_index)
+
+# pickle them
 eq_data.to_pickle('./historyDF.pkl')
+eq_info.to_pickle('./infoDF.pkl')
